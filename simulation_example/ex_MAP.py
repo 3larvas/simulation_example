0efa57c2916f559a57e5c6b4053b3d7e65c3d9ce
3larvas <3larvas@gmail.com>
2020-04-03 00:53:45 +0900

    add link

--- simulation_example/ex_MAP.py
+++ simulation_example/ex_MAP.py
@@ -1,15 +1,18 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
+# geopandas 패키지 설치 방법... 좀 복잡함...
 # https://codedragon.tistory.com/9556
 # https://pypi.org/project/geopandas/
+# geopandas 패키지 설치 후 설치
 # pip install descartes
+
 plt.rcParams["font.family"] = 'NamumGothic'
 plt.rcParams["figure.figsize"] = (10, 10)
 
 file_nm = "HDMap/A1_LANE.shp"
 k_city = gpd.read_file(file_nm)
 k_city
-ax = k_city.convex_hull.plot(color='purple', edgecolor="w")
+ax = k_city.plot(color='purple', edgecolor="b")
 ax.set_title("k-city")
 ax.set_axis_off()
 plt.show()
