c163fcb91daa5c61929cbe46ffc47f25166068e0
3larvas <3larvas@gmail.com>
2020-04-02 23:11:17 +0900

    read shp file

--- simulation_example/ex_MAP.py
+++ simulation_example/ex_MAP.py
@@ -1,13 +1,15 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
-
+# https://codedragon.tistory.com/9556
+# https://pypi.org/project/geopandas/
+# pip install descartes
 plt.rcParams["font.family"] = 'NamumGothic'
 plt.rcParams["figure.figsize"] = (10, 10)
 
 file_nm = "HDMap/A1_LANE.shp"
 k_city = gpd.read_file(file_nm)
-
+k_city
 ax = k_city.convex_hull.plot(color='purple', edgecolor="w")
 ax.set_title("k-city")
-ax.set_axis_iff()
+ax.set_axis_off()
 plt.show()
