57a46c8fc5e3b26eff065e35792808a3e40d599f
3larvas <3larvas@gmail.com>
2020-05-01 19:58:34 +0900

    pure pursuit update

--- simulation_example/ex_pure_pursuit.py
+++ simulation_example/ex_pure_pursuit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*-coding: utf-8-*-
+# -*-coding: utf-8-*-
 import serial
 import time
 import cv2
@@ -9,17 +9,17 @@ import numpy as np
 
 from time import sleep
 
-# ser = serial.Serial('/dev/ttyUSB1',115200)
+ser = serial.Serial('/dev/ttyUSB1', 115200)
 
 # Parameters
 k = 0.1  # look forward gain
-Lfc = 3.0  # [m] look-ahead distance
+Lfc = 0.7  # [m] look-ahead distance
 Kp = 1.0  # speed proportional gain
-dt = 0.05  # [s] time tick
+dt = 0.005  # [s] time tick
 WB = 0.5  # [m] wheel base of vehicle
 
-
 show_animation = True
+
 
 # 차량 상태
 class State:
@@ -109,6 +109,7 @@ class TargetCourse:
 
         return ind, Lf
 
+
 # 조향각 계산 함수
 def pure_pursuit_steer_control(state, trajectory, pind):
     ind, Lf = trajectory.search_target_index(state)
@@ -146,7 +147,7 @@ def plot_arrow(x, y, yaw, length=1.0, width=0.5, fc="r", ec="k"):
 
 
 def func(x):
-	pass
+    pass
 
 
 def clc_find(num):
@@ -167,48 +168,49 @@ def clc_find(num):
     # make list to string
     result = ''.join(digit_num)
     # return 8-digit string to int value
-    return int(result,2)
+    return int(result, 2)
 
 
 def serWrite(v, d):
-	i=0
-	data=[]
+    i = 0
+    data = []
     # 제어패킷 생성
-	writeBuffer(data, v, d)
-    # print(v)
-    # print(d)
+    writeBuffer(data, v, d)
+    print(v)
+    print(d)
     # 제어패킷 전송
-	#print(data)
-	# ser.write(data)
-	cv2.waitKey(25)
+    # print(data)
+    ser.write(data)
+    cv2.waitKey(25)
+
 
 def writeBuffer(data, speed, steering):
     # 기어 기본값 0: 전진, 1:후진
-	direction = 0
+    direction = 0
     # 속도
-	speed_Lo = speed & 0xFF;
-	speed_Hi = speed >> 8;
+    speed_Lo = speed & 0xFF;
+    speed_Hi = speed >> 8;
     # 조향각
-	steering_Lo = steering & 0xFF;
-	steering_Hi = steering >> 8;
+    steering_Lo = steering & 0xFF;
+    steering_Hi = steering >> 8;
     # CLC 계산
-	sum_1  = direction+speed_Lo+speed_Hi+steering_Lo+steering_Hi +220 +5+13+10  ##Fixed Break values (To find the CLC)
-	clc = clc_find(sum_1)
+    sum_1 = direction + speed_Lo + speed_Hi + steering_Lo + steering_Hi + 220 + 5 + 13 + 10  ##Fixed Break values (To find the CLC)
+    clc = clc_find(sum_1)
     # packet 작성
-	data.append(0x53)
-	data.append(0x54)
-	data.append(0x58)
-	data.append(direction)  #0x00 forward #0x01 backward
-	data.append(speed_Lo)  ##speed_Lo
-	data.append(speed_Hi)  ##speed_Hi
-	data.append(steering_Lo)  ##steer_Lo
-	data.append(steering_Hi)  ##steer_Hi
-	data.append(0xDC)  #Brake_L
-	data.append(0x05)  #Brake_H
-	data.append(0x00)  #Encode #default 0
-	data.append(0x0D)
-	data.append(0x0A)
-	data.append(clc)  #CLC
+    data.append(0x53)
+    data.append(0x54)
+    data.append(0x58)
+    data.append(direction)  # 0x00 forward #0x01 backward
+    data.append(speed_Lo)  ##speed_Lo
+    data.append(speed_Hi)  ##speed_Hi
+    data.append(steering_Lo)  ##steer_Lo
+    data.append(steering_Hi)  ##steer_Hi
+    data.append(0xDC)  # Brake_L
+    data.append(0x05)  # Brake_H
+    data.append(0x00)  # Encode #default 0
+    data.append(0x0D)
+    data.append(0x0A)
+    data.append(clc)  # CLC
 
 
 def main():
@@ -217,15 +219,18 @@ def main():
     # cy = [math.sin(ix / 3.0) * ix / 2.0 for ix in cx]
 
     # 경로 정보 입력
-    cx = [0, 5, 10, 30, 31]
-    cy = [0, 10, -15, 0, 0]
+    # cx = [0, 5,  10, 15, 20, 21]
+    # cy = [0, 10, -10, 10, 0,  6]
+    # 경로 정보 입력
+    cx = [0, 0.5, 1, 1.5, 2, 2.5, 5, 6]
+    cy = [0, 0.5, 0.5, 0.5, 2.5, 2.5, 2.5, 2.5]
     # 목표 속도 설정
-    target_speed = 10.0 / 3.6  # [m/s]
+    target_speed = 0.6
     T = 100.0  # max simulation time
 
     # 시작 지점 설정
-    state = State(x=-0.0, y=-6.0, yaw=90.0, v=0.0)
-    
+    state = State(x=-0.0, y=-3.0, yaw=90.0, v=0.0)
+
     lastIndex = len(cx) - 1
     time = 0.0
     states = States()
@@ -245,18 +250,19 @@ def main():
         time += dt
         states.append(time, state)
         ## -------------------- 시리얼 통신 -------------------- ##
-	    # print("di" , di)
+        print("di", di * 180 / 3.141592)  # di : radian -> degree
         # 제어패킷 전송
         # state.v * 3.6 = km/h 변환
         # * 10 값은 제어패킷 속도값(0~800)에 맞추기위한 임의의 상수 값, 추후 변환표를 보고 수정필요
         # di 단위 확인필요
         # 1500 -(di*700) 값은 제어패킷 조향값(1200~1500~1900)에 맞추기 위한 임의의 상소, 추후 변환표를 보고 수정필요
-        serWrite(int(state.v * 3.6 * 10), 1500-int(di * 700))
+        print("vi", state.v)
+        serWrite(int((state.v * 400)), 1550 - int((di * 180 / 3.141592) * 35 / 3))
         # 패킷 간 딜레이 설정
-        # sleep(0.05)
-        ## -------------------------------#-------------------- ##
+        sleep(0.015)
+        ## --------------------------------------------------- ##
         # 움직이는 과정을 그래프 상에 표현, 실제 주행 명령 패킷 전송시 주석처리
-
+        '''
         if show_animation:  # pragma: no cover
             plt.cla()
             # for stopping simulation with the esc key.
@@ -271,7 +277,7 @@ def main():
             plt.grid(True)
             plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4])
             plt.pause(0.00001)
-
+        '''
     # Test
     assert lastIndex >= target_ind, "Cannot goal"
 
@@ -291,6 +297,7 @@ def main():
         plt.ylabel("Speed[km/h]")
         plt.grid(True)
         plt.show()
+
 
 if __name__ == '__main__':
     print("Pure pursuit path tracking simulation start")
